6e0f471b9206a8b7473b970fea5aa001e07e6384
AxasSoft <44229109+AxasSoft@users.noreply.github.com>
2021-06-10 11:04:32 +0300

    "main"

--- app/src/main/java/ru/wood/cuber/MainActivity.kt
+++ app/src/main/java/ru/wood/cuber/MainActivity.kt
@@ -18,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import org.apache.poi.hpsf.Util
 import ru.wood.cuber.databinding.ActivityMainBinding
 import ru.wood.cuber.utill.Utill
+//Ramires//
 //Ramires
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
